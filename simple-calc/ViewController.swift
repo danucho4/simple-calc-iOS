9abbe3b49e8d8bfb26478a16f0729532a7901417
danucho4 <Danucho@Danuchos-MacBook-Pro.local>
2016-12-06 22:32:08 -0800

    Initial Commit

---
+++ simple-calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simple-calc
+//
+//  Created by Daniel Gil Chávez on 12/6/16.
+//  Copyright © 2016 Daniel Gil Chavez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
